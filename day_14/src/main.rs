e272f4881aed7e41c8f74d8c791f1b3af149f77d
Maximilian Stock <mxmlnstock@googlemail.com>
2021-12-16 20:41:53 +0100

    refactor: Day 14

--- day_14/src/main.rs
+++ day_14/src/main.rs
@@ -13,38 +13,35 @@ fn main() {
 }
 
 fn solve(start: Vec<char>, mapping: &HashMap<(char, char), char>, iterations: usize) -> usize {
-    let mut pair_counts = start
-        .windows(2)
-        .map(|w| {
-            let left = *w.first().unwrap();
-            let right = *w.last().unwrap();
-            (left, right)
-        })
-        .fold(HashMap::new(), |mut acc, next| {
-            *acc.entry(next).or_insert(0) += 1;
-            acc
-        });
+    let mut pair_counts =
+        start
+            .windows(2)
+            .map(|w| (w[0], w[1]))
+            .fold(HashMap::new(), |mut acc, next| {
+                *acc.entry(next).or_insert(0) += 1;
+                acc
+            });
 
     let mut total_counts = HashMap::<char, usize>::new();
-    *total_counts.entry(*start.last().unwrap()).or_default() += 1;
 
     for _ in 0..iterations {
-        let mut update = HashMap::new();
-        for ((left, right), v) in pair_counts {
-            if let Some(m) = mapping.get(&(left, right)) {
-                let new_pair = (left, *m);
-                let new_pair2 = (*m, right);
+        pair_counts =
+            pair_counts
+                .into_iter()
+                .fold(HashMap::new(), |mut acc, ((left, right), count)| {
+                    if let Some(m) = mapping.get(&(left, right)) {
+                        *acc.entry((left, *m)).or_insert(0) += count;
+                        *acc.entry((*m, right)).or_insert(0) += count;
+                    }
 
-                *update.entry(new_pair).or_insert(0) += v;
-                *update.entry(new_pair2).or_insert(0) += v;
-            }
-        }
-        pair_counts = update;
+                    acc
+                });
     }
 
     for ((left, _), count) in pair_counts {
         *total_counts.entry(left).or_insert(0) += count;
     }
+    *total_counts.entry(*start.last().unwrap()).or_default() += 1;
 
     let min = total_counts.iter().min_by_key(|e| e.1).unwrap().1;
     let max = total_counts.iter().max_by_key(|e| e.1).unwrap().1;
@@ -52,17 +49,14 @@ fn solve(start: Vec<char>, mapping: &HashMap<(char, char), char>, iterations: us
 }
 
 fn parse_input(input: &str) -> (Vec<char>, HashMap<(char, char), char>) {
-    let mut parts = input.split("\n\n");
-    let start = parts.next().unwrap().chars().collect();
-    let mapping = parts
-        .next()
-        .unwrap()
-        .lines()
+    let mut lines = input.lines();
+    let start = lines.next().unwrap().chars().collect();
+    let mapping = lines
+        .skip(1)
         .map(|l| {
-            let mut parts = l.trim().split(" -> ");
-            let mut left = parts.next().unwrap().chars();
-            let left = (left.next().unwrap(), left.next().unwrap());
-            (left, parts.next().unwrap().chars().next().unwrap())
+            let (left, right) = l.trim().split_once(" -> ").unwrap();
+            let left = (left.chars().next().unwrap(), left.chars().nth(1).unwrap());
+            (left, right.chars().next().unwrap())
         })
         .collect();
 
